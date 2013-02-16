115092010564ab0662d72871545fc7b52c9a6111
sunpshineppp <sunpshineppp@gmail.com>
2013-02-16 23:25:41 +0800

    //TEST GIT COMMIT 20130216 23:24 PC

--- android_lesson/src/com/example/android_lesson/StartingPoing.java
+++ android_lesson/src/com/example/android_lesson/StartingPoing.java
@@ -7,14 +7,16 @@ import android.view.Menu;
 public class StartingPoing extends Activity {
 
 	@Override
-	//TEST GIT COMMIT 20130216 13:58
+	//TEST GIT COMMIT 20130216 13:58 NB
+	//TEST GIT COMMIT 20130216 23:24 PC
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_starting_poing);
 	}
 
 	@Override
-	//TEST GIT COMMIT 20130216 13:58
+	//TEST GIT COMMIT 20130216 13:58 NB
+	//TEST GIT COMMIT 20130216 23:24 PC
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_starting_poing, menu);
