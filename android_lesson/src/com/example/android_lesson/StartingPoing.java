afa606295bd0f7f3012711fb1738a9113a2e5156
sunpshineppp <sunpshineppp@gmail.com>
2013-02-19 21:25:50 +0800

    Tutorial - 13 - Manifest.xml , LAUNCHER

--- android_lesson/src/com/example/android_lesson/StartingPoing.java
+++ android_lesson/src/com/example/android_lesson/StartingPoing.java
@@ -2,7 +2,6 @@ package com.example.android_lesson;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
