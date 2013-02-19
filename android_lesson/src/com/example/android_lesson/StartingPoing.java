cbb9173e7b7f80ef24e7dfbf875a5351d9d15223
sunpshineppp <sunpshineppp@gmail.com>
2013-02-19 18:45:04 +0800

    Tutorial - 8
    http://www.youtube.com/watch?v=eKXnQ83RU3I
    
    http://hatsukiakio.blogspot.tw/2009/05/findviewbyidnullandroid.html
    
    http://blog.csdn.net/harry_helei/article/details/5911658
    
    http://www.2cto.com/kf/201204/127405.html
    
    http://developer.android.com/reference/android/view/View.html

--- android_lesson/src/com/example/android_lesson/StartingPoing.java
+++ android_lesson/src/com/example/android_lesson/StartingPoing.java
@@ -3,24 +3,23 @@ package com.example.android_lesson;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class StartingPoing extends Activity {
-
+	
+	int counter;
+	Button add;
+	Button substract;
+	TextView result;
+	
 	@Override
-	//TEST GIT COMMIT 20130216 13:58 NB
-	//TEST GIT COMMIT 20130216 23:24 PC
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_starting_poing);
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_starting_poing);
+	counter = 0;
+	add = (Button) findViewById(R.id.buADD);
+	substract = (Button) findViewById(R.id.buSUBSTRACT);
+	result = (TextView) findViewById(R.id.resultDisplay);
 	}
-
-	@Override
-	//TEST GIT COMMIT 20130216 13:58 NB
-	//TEST GIT COMMIT 20130216 23:24 PC
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_starting_poing, menu);
-		return true;
-	}
-
 }
