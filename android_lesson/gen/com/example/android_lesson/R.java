c38a357436c6e09b40bfc35aa0292dce709e85de
sunpshineppp <sunpshineppp@gmail.com>
2013-02-19 20:59:23 +0800

    Tutorial - 11 - background
    http://www.youtube.com/watch?v=IHg_0HJ5iQo

--- android_lesson/gen/com/example/android_lesson/R.java
+++ android_lesson/gen/com/example/android_lesson/R.java
@@ -12,23 +12,28 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int opening_background=0x7f020001;
     }
     public static final class id {
-        public static final int buADD=0x7f070001;
-        public static final int buSUBSTRACT=0x7f070002;
-        public static final int menu_settings=0x7f070003;
-        public static final int resultDisplay=0x7f070000;
+        public static final int buADD=0x7f080001;
+        public static final int buSUBSTRACT=0x7f080002;
+        public static final int menu_settings=0x7f080003;
+        public static final int resultDisplay=0x7f080000;
     }
     public static final class layout {
         public static final int activity_starting_poing=0x7f030000;
+        public static final int opening=0x7f030001;
     }
     public static final class menu {
-        public static final int activity_starting_poing=0x7f060000;
+        public static final int activity_starting_poing=0x7f070000;
+    }
+    public static final class raw {
+        public static final int time_after_time=0x7f040000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
+        public static final int app_name=0x7f050000;
+        public static final int hello_world=0x7f050001;
+        public static final int menu_settings=0x7f050002;
     }
     public static final class style {
         /** 
@@ -52,10 +57,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
