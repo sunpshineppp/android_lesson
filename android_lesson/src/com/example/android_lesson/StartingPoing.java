1a4a4f63bd22e88f382e66f7cd653309ce40e7fb
sunpshineppp <sunpshineppp@gmail.com>
2013-02-19 19:08:59 +0800

    Tutorial - 9 - onClickListener
    http://www.youtube.com/watch?v=WjE-pWYElsE
    
    http://disp.cc/b/27-31Zq
    
    http://www.jollen.org/blog/2009/06/jollen-android-programming-16.html
    
    http://blog.csdn.net/dickren123/article/details/7216975
    
    http://www.eoeandroid.com/blog-548102-1689.html
    
    http://developer.android.com/reference/android/view/View.OnClickListener.html

--- android_lesson/src/com/example/android_lesson/StartingPoing.java
+++ android_lesson/src/com/example/android_lesson/StartingPoing.java
@@ -3,6 +3,7 @@ package com.example.android_lesson;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -21,5 +22,20 @@ public class StartingPoing extends Activity {
 	add = (Button) findViewById(R.id.buADD);
 	substract = (Button) findViewById(R.id.buSUBSTRACT);
 	result = (TextView) findViewById(R.id.resultDisplay);
+	
+	//use setOnClickListener , onClick
+	add.setOnClickListener(new View.OnClickListener() {
+		public void onClick(View v){
+		//set actions after a CLICK buADD
+			
+		}
+	});
+	substract.setOnClickListener(new View.OnClickListener() {
+		public void onClick(View v){
+		//set actions after a CLICK buSUBSTRACT
+			
+		}
+	});
+	
 	}
 }
