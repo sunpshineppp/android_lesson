cdb00b20c246cd5587458192dcc8c73d73888f1a
sunpshineppp <sunpshineppp@gmail.com>
2013-02-19 21:10:07 +0800

    Tutorial - 12 - extends Activity , void onCreate , setContentView
    http://www.youtube.com/watch?v=H92G3CpSQf4

---
+++ android_lesson/src/com/example/android_lesson/Opening.java
@@ -0,0 +1,18 @@
+package com.example.android_lesson;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Opening extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.opening);
+		
+	}
+	
+
+}
