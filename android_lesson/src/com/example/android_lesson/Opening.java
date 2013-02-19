a6e3e26a27c6df735db9100f45e12e1ebe8e9f13
sunpshineppp <sunpshineppp@gmail.com>
2013-02-19 22:05:44 +0800

    Tutorial - 14,15 - Thread , try catch finally , Thread start() , InterruptedException .printStackTrace
    http://www.youtube.com/watch?v=hy0mRoT1ZlM
    
    http://www.youtube.com/watch?v=Xpkbu2GrJpE

--- android_lesson/src/com/example/android_lesson/Opening.java
+++ android_lesson/src/com/example/android_lesson/Opening.java
@@ -1,6 +1,7 @@
 package com.example.android_lesson;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Opening extends Activity{
@@ -11,7 +12,23 @@ public class Opening extends Activity{
 		super.onCreate(savedInstanceState);
 		
 		setContentView(R.layout.opening);
+				
+		Thread timer = new Thread(){
+			
+			public void run(){
+				try{
+					sleep(5000);				
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} finally {
+					Intent goStartPoing = new Intent("com.example.android_lesson.STARTINGPOINT");
+					startActivity(goStartPoing);
+				}
+			}
+			
+		};
 		
+		timer.start();
 	}
 	
 
