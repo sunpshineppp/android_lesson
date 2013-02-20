92f2e96d237eb78ad8385d13423a22bad7572a96
sunpshineppp <sunpshineppp@gmail.com>
2013-02-20 23:46:56 +0800

    Tutorial - 26,27 - setOnClickListener, View.OnClickListener, onClick(), setInputType
    http://www.youtube.com/watch?v=NyusGsXc6SQ 
    
    http://www.youtube.com/watch?v=_joTj5XTwuQ

--- android_lesson/src/com/example/android_lesson/Text.java
+++ android_lesson/src/com/example/android_lesson/Text.java
@@ -2,6 +2,8 @@ package com.example.android_lesson;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -17,9 +19,23 @@ public class Text extends Activity {
 		setContentView(R.layout.text);
 		
 		Button getTextInput = (Button) findViewById(R.id.buSetTextInput);
-		ToggleButton switchPassword = (ToggleButton) findViewById(R.id.tbuSwitchPassword);
-		EditText textIn = (EditText) findViewById(R.id.textInput);
+		final ToggleButton switchPassword = (ToggleButton) findViewById(R.id.tbuSwitchPassword);
+		final EditText textIn = (EditText) findViewById(R.id.textInput);
 		TextView textOut = (TextView) findViewById(R.id.textOutput);
+		
+		switchPassword.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				
+				if (switchPassword.isChecked()) {
+					textIn.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+				} else{
+					textIn.setInputType(InputType.TYPE_CLASS_TEXT);
+				}
+				
+			}
+			
+		});
 		
 	}
 	
