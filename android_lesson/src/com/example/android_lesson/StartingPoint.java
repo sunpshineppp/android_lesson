51d5096117a356e59ca6d41b4e6d9c125180c01d
sunpshineppp <sunpshineppp@gmail.com>
2013-03-15 14:29:13 +0800

    Tutorial - 58 - change variables by preferences

--- android_lesson/src/com/example/android_lesson/StartingPoint.java
+++ android_lesson/src/com/example/android_lesson/StartingPoint.java
@@ -1,7 +1,9 @@
 package com.example.android_lesson;
 
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,7 +15,16 @@ public class StartingPoint extends Activity {
 	int counter;
 	Button add;
 	Button substract;
+	
+	TextView welcome;
 	TextView result;
+	TextView count;
+	
+	String UserName;
+	String welcomeMessage;
+	
+	String ListItemSelected;
+	String resultCount;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +33,26 @@ public class StartingPoint extends Activity {
 	counter = 0;
 	add = (Button) findViewById(R.id.buADD);
 	substract = (Button) findViewById(R.id.buSUBSTRACT);
+	
+	welcome = (TextView) findViewById (R.id.startingPointWelcome);
 	result = (TextView) findViewById(R.id.resultDisplay2);
+	count = (TextView) findViewById(R.id.count);
+	
+	// set welcome by EditText input from preferences
+	SharedPreferences getUserName = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+	UserName = getUserName.getString("userName","Stranger");
+	welcomeMessage = "Hi, " + UserName + ", your total is.." ;
+	welcome.setText(welcomeMessage);
+	
+	// set result by List input from preferences
+	SharedPreferences getListItemSelected = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+	ListItemSelected = getListItemSelected.getString("list","1");
+	if(ListItemSelected.contentEquals("1")){
+		count.setText("USD");
+	}
+	if(ListItemSelected.contentEquals("2")){
+		count.setText("NTD");
+	}
 	
 	//use setOnClickListener , onClick
 	add.setOnClickListener(new View.OnClickListener() {
