74f6103b7ada243f517bb6d66a1d8f15ec6e6e77
sunpshineppp <sunpshineppp@gmail.com>
2013-03-23 11:10:30 +0800

    Tutorial - 78 - WakeLock
    http://www.youtube.com/watch?v=3r9NGjBvv2w

--- android_lesson/src/com/example/android_lesson/BallBall.java
+++ android_lesson/src/com/example/android_lesson/BallBall.java
@@ -1,20 +1,47 @@
 package com.example.android_lesson;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
 
+@SuppressLint("Wakelock")
 public class BallBall extends Activity{
 	
 	CreateBallBall GiveMeTheDammBall ;
+	// declare an wake lock
+	WakeLock locks;
 
+	@SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
+		
+		// set an wake lock to keep it open, have the device stay on.
+		PowerManager power = (PowerManager) getSystemService(Context.POWER_SERVICE);
+		locks = power.newWakeLock(PowerManager.FULL_WAKE_LOCK ,"power saving");
+		
+		
 		super.onCreate(savedInstanceState);
+		
+		// activated the wake lock
+		locks.acquire();
 		
 		// call another class to control the view
 		GiveMeTheDammBall = new CreateBallBall(this);
 		setContentView(GiveMeTheDammBall);
+		
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		
+		// break the lock.
+		locks.release();
 		
 	}
 
